67dd606186ab186976b5dd68cafab8446e83f11b
SURAJITSHAW <piklu3043@gmail.com>
2024-11-10 20:48:04 +0530

    Category fragment done

--- app/src/main/java/com/example/recipeapp/Fragments/CatFragment.kt
+++ app/src/main/java/com/example/recipeapp/Fragments/CatFragment.kt
@@ -1,60 +1,62 @@
 package com.example.recipeapp.Fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.recipeapp.Activities.CategoryWiseRecipeActivity
+import com.example.recipeapp.Fragments.HomeFragment.Companion.CATEGORY_NAME
+import com.example.recipeapp.Models.Category
 import com.example.recipeapp.R
+import com.example.recipeapp.adapter.CategoryAdapter
+import com.example.recipeapp.databinding.FragmentCatBinding
+import com.example.recipeapp.databinding.FragmentHomeBinding
+import com.example.recipeapp.viewModels.HomeViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class CatFragment : Fragment(), CategoryAdapter.OnItemClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CatFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class CatFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val viewModel: HomeViewModel by viewModels()
+
+
+    private var _binding: FragmentCatBinding? = null
+    private val binding get() = _binding!! // Use binding reference to access views
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cat, container, false)
+        _binding = FragmentCatBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CatFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CatFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpCategoriesRecyclerView()
+    }
+
+    private fun setUpCategoriesRecyclerView() {
+//        call the method in viewmodel to get the trending meals
+        viewModel.getCategories()
+//        observe the livedate
+        viewModel.categoriesLiveData.observe(viewLifecycleOwner) { catList ->
+            if (catList != null) {
+                binding.categoryRecyclerView.layoutManager = GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
+                binding.categoryRecyclerView.adapter = CategoryAdapter(catList, this)
             }
+        }
+    }
+
+    override fun onItemClick(category: Category) {
+        val intent = Intent(requireContext(), CategoryWiseRecipeActivity::class.java)
+        intent.putExtra(CATEGORY_NAME, category.strCategory)
+        startActivity(intent)
     }
 }
